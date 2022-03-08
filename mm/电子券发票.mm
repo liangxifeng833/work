21ff2fb89de8096a4e524c7ab8acf668d4dba25b
liangxifeng <liangxifeng833@163.com>
2022-03-08 17:37:00 +0800

    电子券数据结构修改

---
+++ mm/电子券发票.mm
@@ -0,0 +1,45 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1646362840255" ID="ID_815066544" MODIFIED="1646362849997" TEXT="&#x7535;&#x5b50;&#x5238;&#x53d1;&#x7968;">
+<node CREATED="1646362851338" FOLDED="true" ID="ID_1925397188" MODIFIED="1646363165308" POSITION="right" STYLE="fork" TEXT="&#x5408;&#x540c;ticket _pay_record&#x8868;&#x65b0;&#x589e;&#x5b57;&#x6bb5;">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1646363036255" ID="ID_111416137" MODIFIED="1646363049703" TEXT="&#x53d1;&#x7968;&#x53f7;"/>
+<node CREATED="1646363050180" ID="ID_1828399252" MODIFIED="1646363054699" TEXT="&#x5ba1;&#x6838;&#x72b6;&#x6001;"/>
+</node>
+<node CREATED="1646363058996" ID="ID_72431711" MODIFIED="1646363159671" POSITION="right" TEXT="&#x6536;&#x94f6;&#x7cfb;&#x7edf;&#x9000;&#x6b3e;&#x9700;&#x8981;&#x51b2;&#x7ea2;&#x65b0;&#x589e;&#x52a0;&#x7684;&#x5b57;&#x6bb5;">
+<node CREATED="1646386676019" ID="ID_308702018" MODIFIED="1646386686014" TEXT="action/casher/RefundAbstract.php"/>
+</node>
+<node CREATED="1646643576026" ID="ID_561589786" MODIFIED="1646727623950" POSITION="right" TEXT="BLL&#x4fee;&#x6539;">
+<node CREATED="1646643582386" ID="ID_1392746589" MODIFIED="1646643670441" TEXT="bll/application/libraries/action/casher/PayTypeAction.php">
+<node CREATED="1646643666098" ID="ID_973282578" MODIFIED="1646643666455" TEXT="https://docs.qq.com/doc/DSk1GWWdTRkdtVW9v"/>
+<node CREATED="1646643680709" ID="ID_641796415" MODIFIED="1646643693967" TEXT="&#x53bb;&#x6389;&#x8d2d;&#x7269;&#x5238;&#x76f8;&#x5173;&#x67e5;&#x8be2;"/>
+<node CREATED="1646643694476" ID="ID_383875143" MODIFIED="1646643735302" TEXT="&#x6dfb;&#x52a0;&#x67e5;&#x8be2;&#x7535;&#x5b50;&#x5238;&#x624b;&#x7eed;&#x8d39;&#x7a0b;&#x5e8f;(&#x5546;&#x6237;&#x5206;&#x644a;&#x91d1;&#x989d;)"/>
+</node>
+<node CREATED="1646729349293" ID="ID_1123661857" MODIFIED="1646729377515" TEXT="&#x6536;&#x94f6;&#x7cfb;&#x7edf;&#x9000;&#x5238;&#xff0c;&#x9700;&#x8981;&#x5c06;&#x5408;&#x540c;&#x6536;&#x5238;&#x8868;&#x8d1f;&#x5411;&#x65b0;&#x589e;&#x5b57;&#x6bb5;&#x51b2;&#x7ea2;">
+<node CREATED="1646729398978" ID="ID_1243458861" MODIFIED="1646729581527" TEXT="application/libraries/action/casher/RefundAbstract.php "/>
+<node CREATED="1646729589628" ID="ID_782446941" MODIFIED="1646729699864" TEXT="320&#x884c;&#x7684;getRecord()&#x65b9;&#x6cd5;&#xff0c;&#x5c06;&#x65b0;&#x589e;&#x5b57;&#x6bb5;&#x51b2;&#x7ea2;&#x6570;&#x636e;&#x8865;&#x5145;&#x5b8c;&#x6574;"/>
+</node>
+</node>
+<node CREATED="1646728571691" ID="ID_987754763" MODIFIED="1646728573907" POSITION="right" TEXT="DAL&#x4fee;&#x6539;">
+<node CREATED="1646728574684" ID="ID_604265550" MODIFIED="1646728974902" TEXT="&#x6536;&#x94f6;&#x7cfb;&#x7edf;&#x6536;&#x5238;&#x65f6;&#x5019;&#xff0c;&#x5411;&#x5408;&#x540c;&#x6536;&#x6b3e;&#x8868;&#x65b0;&#x589e;&#x6570;&#x636e;&#xff1a;&#x5356;&#x573a;&#x5206;&#x644a;&#x6bd4;&#xff0c;&#x5356;&#x573a;&#x5206;&#x644a;&#x91d1;&#x989d;&#xff0c;&#x5546;&#x6237;&#x5206;&#x644a;&#x6bd4;&#x5b57;&#x6bb5;&#xff0c;&#x6ce8;&#x610f;&#xff1a;&#x8fd9;&#x91cc;&#x5c55;&#x4f4d;&#x53f7;&#x5b57;&#x6bb5;&#x65e0;&#x9700;&#x65b0;&#x589e;&#xff08;&#x56e0;&#x4e3a;&#x8fd9;&#x662f;&#x5c01;&#x9876;&#x989d;&#x9879;&#x76ee;&#x9700;&#x8981;&#x7684;&#xff09;">
+<node CREATED="1646728582782" ID="ID_260887581" MODIFIED="1646728780579" TEXT="application/controllers/ticket/tctChangeTicketState.php"/>
+<node CREATED="1646728793845" ID="ID_572685691" MODIFIED="1646728814923" TEXT="&#x7a0b;&#x5e8f;133&#x884c;&#x53ef;&#x4ee5;&#x53d6;&#x5230;&#x65b0;&#x589e;&#x7684;&#x6570;&#x636e;"/>
+<node CREATED="1646729162814" ID="ID_1791789621" MODIFIED="1646729180418" TEXT="&#x5176;&#x4e2d;&#x5356;&#x573a;&#x5206;&#x644a;&#x989d;=&#x5238;&#x9762;&#x989d;-&#x5546;&#x6237;&#x5206;&#x644a;&#x91d1;&#x989d;"/>
+</node>
+</node>
+<node CREATED="1646721995280" ID="ID_290813313" MODIFIED="1646723259865" POSITION="right" TEXT="&#x65b0;&#x63a5;&#x53e3;">
+<node CREATED="1646722002468" ID="ID_196473584" MODIFIED="1646722229122" TEXT="&#x63d2;&#x5361;&#x4eba;&#x5b57;&#x6bb5;&#xff1a;merchant_info.merchant_bank_man"/>
+<node CREATED="1646723269112" ID="ID_840727433" MODIFIED="1646727002761" TEXT="BLL&#x65b0;&#x589e;">
+<node CREATED="1646723244438" ID="ID_928519659" MODIFIED="1646727195111" TEXT="&#x901a;&#x8fc7;K&#x4ee3;&#x7801;&#x8bfb;&#x53d6;&#x5546;&#x6237;&#x6570;&#x636e;&#x8bfb;&#x53d6;&#x63a5;&#x53e3;">
+<node CREATED="1646727004604" ID="ID_1747187400" MODIFIED="1646730623070" TEXT="common/getMerchantInfoByKid&#x8bfb;&#x53d6;action/common/GetMerchantInfoByKid"/>
+<node CREATED="1646727394497" ID="ID_965009983" MODIFIED="1646727402629" TEXT="&#x5165;&#x53c2;&#x4e3a;&#x5546;&#x6237;K&#x4ee3;&#x7801;"/>
+<node CREATED="1646727167731" ID="ID_41200353" MODIFIED="1646727180685" TEXT="&#x8fd4;&#x56de;merchant_info&#x8868;&#x6240;&#x6709;&#x6570;&#x636e;"/>
+</node>
+<node CREATED="1646727242177" ID="ID_1910331740" MODIFIED="1646727306356" TEXT="&#x5408;&#x540c;&#x6536;&#x5238;&#x5217;&#x8868;">
+<node CREATED="1646727322181" ID="ID_211408362" MODIFIED="1646731300401" TEXT="ticket/getTicketPayRecordList"/>
+</node>
+</node>
+</node>
+</node>
+</map>
