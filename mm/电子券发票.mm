5cb2d086f2d06d166bc12a8329af9be4ff5489cf
liangxifeng <liangxifeng833@163.com>
2022-03-11 18:01:24 +0800

    12

--- mm/电子券发票.mm
+++ mm/电子券发票.mm
@@ -1,47 +1,44 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1646362840255" ID="ID_815066544" MODIFIED="1646362849997" TEXT="&#x7535;&#x5b50;&#x5238;&#x53d1;&#x7968;">
-<node CREATED="1646643576026" ID="ID_561589786" MODIFIED="1646727623950" POSITION="right" TEXT="BLL&#x4fee;&#x6539;">
-<node CREATED="1646643582386" ID="ID_1392746589" MODIFIED="1646643670441" TEXT="bll/application/libraries/action/casher/PayTypeAction.php">
-<node CREATED="1646643666098" ID="ID_973282578" MODIFIED="1646643666455" TEXT="https://docs.qq.com/doc/DSk1GWWdTRkdtVW9v"/>
-<node CREATED="1646643680709" ID="ID_641796415" MODIFIED="1646643693967" TEXT="&#x53bb;&#x6389;&#x8d2d;&#x7269;&#x5238;&#x76f8;&#x5173;&#x67e5;&#x8be2;"/>
-<node CREATED="1646643694476" ID="ID_383875143" MODIFIED="1646643735302" TEXT="&#x6dfb;&#x52a0;&#x67e5;&#x8be2;&#x7535;&#x5b50;&#x5238;&#x624b;&#x7eed;&#x8d39;&#x7a0b;&#x5e8f;(&#x5546;&#x6237;&#x5206;&#x644a;&#x91d1;&#x989d;)"/>
+<node CREATED="1646965324953" ID="ID_1071551423" MODIFIED="1646966468455" POSITION="right" TEXT="&#x9875;&#x9762;">
+<node CREATED="1646965335832" ID="ID_697325764" MODIFIED="1646965343884" TEXT="&#x5bf9;&#x8d26;&#x7cfb;&#x7edf;">
+<node CREATED="1646966411932" ID="ID_539419384" MODIFIED="1646966414639" TEXT="&#x4fee;&#x6539;">
+<node CREATED="1646965981497" ID="ID_1992060786" MODIFIED="1646966438299" TEXT="&#x5df2;&#x786e;&#x8ba4;&#x8d26;&#x5355;">
+<node CREATED="1646966015019" ID="ID_724551939" MODIFIED="1646966097021" TEXT="&#x652f;&#x4ed8;&#x660e;&#x7ec6;&#x9875;&#x9762;&#xff0c;&#x9700;&#x8981;&#x5c06;&#x7535;&#x5b50;&#x5238;&#x624b;&#x7eed;&#x8d39;&#x8865;&#x5145;&#x5b8c;&#x6574;"/>
+<node CREATED="1646966050033" ID="ID_1931025943" MODIFIED="1646966057976" TEXT="&#x53bb;&#x6389;&#x8d2d;&#x7269;&#x5238;&#x76f8;&#x5173;&#x90e8;&#x5206;&#x4ee3;&#x7801;"/>
 </node>
-<node CREATED="1646729349293" FOLDED="true" ID="ID_1123661857" MODIFIED="1646819805572" TEXT="&#x6536;&#x94f6;&#x7cfb;&#x7edf;&#x9000;&#x5238;&#xff0c;&#x9700;&#x8981;&#x5c06;&#x5408;&#x540c;&#x6536;&#x5238;&#x8868;&#x8d1f;&#x5411;&#x65b0;&#x589e;&#x5b57;&#x6bb5;&#x51b2;&#x7ea2;">
-<icon BUILTIN="button_cancel"/>
-<node CREATED="1646729398978" ID="ID_1243458861" MODIFIED="1646729581527" TEXT="application/libraries/action/casher/RefundAbstract.php "/>
-<node CREATED="1646729589628" ID="ID_782446941" MODIFIED="1646729699864" TEXT="320&#x884c;&#x7684;getRecord()&#x65b9;&#x6cd5;&#xff0c;&#x5c06;&#x65b0;&#x589e;&#x5b57;&#x6bb5;&#x51b2;&#x7ea2;&#x6570;&#x636e;&#x8865;&#x5145;&#x5b8c;&#x6574;"/>
-</node>
-<node CREATED="1646810300117" FOLDED="true" ID="ID_1201329714" MODIFIED="1646819814435" TEXT="&#x6536;&#x94f6;&#x7cfb;&#x7edf;&#x6536;&#x5238;&#x65f6;&#x5019;&#xff0c;&#x8c03;&#x7528;BLL&#x7528;&#x5238;&#x63a5;&#x53e3;useTicket.index_put()&#x65b9;&#x6cd5;&#x4fee;&#x6539;&#xff1a;&#x67e5;&#x8be2;&#x5546;&#x6237;&#x4fe1;&#x606f;">
-<icon BUILTIN="button_cancel"/>
-<node CREATED="1646810530956" ID="ID_1059891947" MODIFIED="1646810531723" TEXT="$this-&gt;_CI-&gt;load-&gt;library(&apos;action/common/MerchantOnlineAction&apos;,&apos;&apos;,&apos;MerchantOnlineAction&apos;);"/>
-<node CREATED="1646810543730" ID="ID_309651394" MODIFIED="1646810556501" TEXT="$merchantArr = $this-&gt;_CI-&gt;MerchantOnlineAction-&gt;get(array(&apos;orer_number&apos;=&gt;&#x5408;&#x540c;&#x53f7;));"/>
-<node CREATED="1646810571636" ID="ID_1586693509" MODIFIED="1646810635700" TEXT="&#x5f97;&#x5230;&#x5546;&#x6237;&#x4fe1;&#x606f;&#x7684;&#x5c55;&#x4f4d;&#x53f7;&#x540e;&#xff0c;&#x5728;&#x8c03;&#x7528;DAL tctCangeTicketState&#x4e8b;&#x52a1;&#x65f6;&#xff0c;&#x5c06;&#x5c55;&#x4f4d;&#x53f7;&#x4f5c;&#x4e3a;&#x5165;&#x53c2;&#x4f20;&#x8fdb;&#x8fdb;&#x53bb;"/>
-</node>
-<node CREATED="1646816286274" ID="ID_1278444760" MODIFIED="1646819819873" TEXT="&#x6536;&#x6b3e;&#x5927;&#x4fdd;&#x5b58;action">
-<node CREATED="1646816329724" ID="ID_1086332265" MODIFIED="1646816354940" TEXT="application/libraries/action/casher/PaymentAbstract.php"/>
-<node CREATED="1646816356757" ID="ID_1967720446" MODIFIED="1646816402878" TEXT="&#x65b0;&#x589e;&#x73b0;&#x91d1;&#x548c;&#x8d2d;&#x7269;&#x5238;&#x652f;&#x4ed8;&#x65b9;&#x5f0f;&#x8868;&#xff0c;&#x65b0;&#x589e;&#x6570;&#x636e;&#x65f6;&#xff0c;&#x5c06;&#x65b0;&#x7684;&#x5b57;&#x6bb5;&#x586b;&#x5165;&#x6709;&#x6548;&#x503c;&#x3002;"/>
-</node>
-<node CREATED="1646819601345" ID="ID_689956558" MODIFIED="1646819822535" TEXT="&#x9000;&#x6b3e;&#x5927;&#x4fdd;&#x5b58;action">
-<node CREATED="1646819696630" ID="ID_841579195" MODIFIED="1646819714170" TEXT="application/libraries/action/casher/RefundAbstract.php"/>
-<node CREATED="1646819715612" ID="ID_448371435" MODIFIED="1646819739849" TEXT="&#x7ec4;&#x7ec7;payfrom_list&#x5305;&#x51b2;&#x7ea2;&#x6570;&#x636e;&#x65f6;&#xff0c;&#x5c06;&#x65b0;&#x52a0;&#x7684;&#x5b57;&#x6bb5;&#x586b;&#x5145;&#x5b8c;&#x6574;"/>
+<node CREATED="1646966075823" ID="ID_453714081" MODIFIED="1646966439255" TEXT="&#x5df2;&#x7ed3;&#x7b97;&#x8d26;&#x5355;">
+<node CREATED="1646966219073" ID="ID_213382356" MODIFIED="1646966238477" TEXT="&#x65b0;&#x589e;&#x8df3;&#x8f6c;&#x652f;&#x4ed8;&#x660e;&#x7ec6;&#x94fe;&#x63a5;"/>
 </node>
 </node>
-<node CREATED="1646728571691" ID="ID_987754763" MODIFIED="1646728573907" POSITION="right" TEXT="DAL&#x4fee;&#x6539;">
-<node CREATED="1646728574684" FOLDED="true" ID="ID_604265550" MODIFIED="1646819831728" TEXT="&#x6536;&#x94f6;&#x7cfb;&#x7edf;&#x6536;&#x5238;&#x65f6;&#x5019;&#xff0c;&#x5411;&#x5408;&#x540c;&#x6536;&#x6b3e;&#x8868;&#x65b0;&#x589e;&#x6570;&#x636e;&#xff1a;&#x5356;&#x573a;&#x5206;&#x644a;&#x6bd4;&#xff0c;&#x5356;&#x573a;&#x5206;&#x644a;&#x91d1;&#x989d;&#xff0c;&#x5546;&#x6237;&#x5206;&#x644a;&#x6bd4;&#x5b57;&#x6bb5;&#xff0c;&#x5c55;&#x4f4d;&#x53f7;">
-<icon BUILTIN="button_cancel"/>
-<node CREATED="1646728582782" ID="ID_260887581" MODIFIED="1646728780579" TEXT="application/controllers/ticket/tctChangeTicketState.php"/>
-<node CREATED="1646728793845" ID="ID_572685691" MODIFIED="1646728814923" TEXT="&#x7a0b;&#x5e8f;133&#x884c;&#x53ef;&#x4ee5;&#x53d6;&#x5230;&#x65b0;&#x589e;&#x7684;&#x6570;&#x636e;"/>
-<node CREATED="1646729162814" ID="ID_1791789621" MODIFIED="1646729180418" TEXT="&#x5176;&#x4e2d;&#x5356;&#x573a;&#x5206;&#x644a;&#x989d;=&#x5238;&#x9762;&#x989d;-&#x5546;&#x6237;&#x5206;&#x644a;&#x91d1;&#x989d;"/>
+<node CREATED="1646966376522" ID="ID_1673916947" MODIFIED="1646966388925" TEXT="&#x65b0;&#x589e;">
+<node CREATED="1646966389410" ID="ID_1280922532" MODIFIED="1646966389849" TEXT="&#x7535;&#x5b50;&#x5238;&#x53d1;&#x7968;&#x5217;&#x8868; "/>
+<node CREATED="1646966391001" ID="ID_1237255538" MODIFIED="1646966400422" TEXT="&#x5f55;&#x5165;&#x53d1;&#x7968;"/>
+<node CREATED="1646966401268" ID="ID_1299534513" MODIFIED="1646966407167" TEXT="&#x53d1;&#x7968;&#x8be6;&#x60c5;"/>
 </node>
-<node CREATED="1646819645766" ID="ID_1402737151" MODIFIED="1646819826242" TEXT="payfrom_list model&#x65b0;&#x589e;&#x5b57;&#x6bb5;"/>
 </node>
-<node CREATED="1646721995280" ID="ID_290813313" MODIFIED="1646723259865" POSITION="right" TEXT="&#x65b0;&#x63a5;&#x53e3;">
-<node CREATED="1646722002468" ID="ID_196473584" MODIFIED="1646722229122" TEXT="&#x63d2;&#x5361;&#x4eba;&#x5b57;&#x6bb5;&#xff1a;merchant_info.merchant_bank_man"/>
+<node CREATED="1646965647899" ID="ID_841406279" MODIFIED="1646965650224" TEXT="mis&#x7cfb;&#x7edf;">
+<node CREATED="1646969592823" ID="ID_1342654813" MODIFIED="1646969599321" TEXT="&#x65b0;&#x589e;">
+<node CREATED="1646966627511" ID="ID_1929870396" MODIFIED="1646966633320" TEXT="&#x53d1;&#x7968;&#x5ba1;&#x6838;">
+<node CREATED="1646969583743" ID="ID_1203641269" MODIFIED="1646969647502" TEXT="&#x590d;&#x7528;&#x5bf9;&#x8d26;&#x7cfb;&#x53d1;&#x7968;&#x5217;&#x8868;&#x63a5;&#x53e3;"/>
+</node>
+<node CREATED="1646966401268" ID="ID_1668618481" MODIFIED="1646966407167" TEXT="&#x53d1;&#x7968;&#x8be6;&#x60c5;">
+<node CREATED="1646969583743" ID="ID_1763655500" MODIFIED="1646969675313" TEXT="&#x590d;&#x7528;&#x5bf9;&#x8d26;&#x7cfb;&#x53d1;&#x7968;&#x8be6;&#x60c5;&#x63a5;&#x53e3;"/>
+</node>
+<node CREATED="1646969713277" ID="ID_1776650734" MODIFIED="1646969728252" TEXT="&#x6709;&#x5ba1;&#x6838;&#x901a;&#x8fc7;&#x529f;&#x80fd;"/>
+<node CREATED="1646969771374" ID="ID_1223027333" MODIFIED="1646969780828" TEXT="&#x6709;&#x6279;&#x91cf;&#x5ba1;&#x6838;&#x901a;&#x8fc7;&#x529f;&#x80fd;"/>
+</node>
+<node CREATED="1646969833413" ID="ID_980145409" MODIFIED="1646969836685" TEXT="&#x4fee;&#x6539;">
+<node CREATED="1646969837355" ID="ID_1402739246" MODIFIED="1646969841592" TEXT="&#x5546;&#x6237;&#x7ed3;&#x7b97;"/>
+</node>
+</node>
+</node>
+<node CREATED="1646721995280" FOLDED="true" ID="ID_290813313" MODIFIED="1646968613011" POSITION="right" TEXT="&#x76f8;&#x5173;&#x63a5;&#x53e3;">
 <node CREATED="1646723269112" ID="ID_840727433" MODIFIED="1646727002761" TEXT="BLL&#x65b0;&#x589e;">
-<node CREATED="1646723244438" ID="ID_928519659" MODIFIED="1646727195111" TEXT="&#x901a;&#x8fc7;K&#x4ee3;&#x7801;&#x8bfb;&#x53d6;&#x5546;&#x6237;&#x6570;&#x636e;&#x8bfb;&#x53d6;&#x63a5;&#x53e3;">
-<node CREATED="1646727004604" ID="ID_1747187400" MODIFIED="1646730623070" TEXT="common/getMerchantInfoByKid&#x8bfb;&#x53d6;action/common/GetMerchantInfoByKid"/>
+<node CREATED="1646723244438" ID="ID_928519659" MODIFIED="1646965220326" TEXT="&#x901a;&#x8fc7;K&#x4ee3;&#x7801;&#x8bfb;&#x53d6;&#x5546;&#x6237;&#x6570;&#x636e;&#x63a5;&#x53e3;">
 <node CREATED="1646727394497" ID="ID_965009983" MODIFIED="1646727402629" TEXT="&#x5165;&#x53c2;&#x4e3a;&#x5546;&#x6237;K&#x4ee3;&#x7801;"/>
+<node CREATED="1646727004604" ID="ID_1747187400" MODIFIED="1646730623070" TEXT="common/getMerchantInfoByKid&#x8bfb;&#x53d6;action/common/GetMerchantInfoByKid"/>
 <node CREATED="1646727167731" ID="ID_41200353" MODIFIED="1646727180685" TEXT="&#x8fd4;&#x56de;merchant_info&#x8868;&#x6240;&#x6709;&#x6570;&#x636e;"/>
 </node>
 <node CREATED="1646727242177" ID="ID_1910331740" MODIFIED="1646819906003" TEXT="&#x67e5;&#x8be2;&#x5408;&#x540c;&#x6536;&#x5238;&#x5217;&#x8868;&#x6570;&#x636e;">
@@ -59,6 +56,45 @@
 <node CREATED="1646899117327" ID="ID_47108266" MODIFIED="1646899125269" TEXT="&#x6279;&#x91cf;&#x5ba1;&#x6838;&#x7535;&#x5b50;&#x5238;&#x53d1;&#x7968; ">
 <node CREATED="1646899128628" ID="ID_1194832586" MODIFIED="1646899129362" TEXT="seller/checkInvoice.batchCheck_post"/>
 </node>
+</node>
+<node CREATED="1646965244157" ID="ID_75106875" MODIFIED="1646965247208" TEXT="BLL&#x4fee;&#x6539;">
+<node CREATED="1646816286274" ID="ID_1278444760" MODIFIED="1646819819873" TEXT="&#x6536;&#x6b3e;&#x5927;&#x4fdd;&#x5b58;action">
+<node CREATED="1646816329724" ID="ID_1086332265" MODIFIED="1646816354940" TEXT="application/libraries/action/casher/PaymentAbstract.php"/>
+<node CREATED="1646816356757" ID="ID_1967720446" MODIFIED="1646967865932" TEXT="&#x65b0;&#x589e;payform_list&#x8868;&#x589e;&#x6570;&#x636e;&#x65f6;&#xff0c;&#x5c06;&#x65b0;&#x7684;&#x5b57;&#x6bb5;&#x586b;&#x5165;&#x6709;&#x6548;&#x503c;&#x5356;&#x573a;&#x5206;&#x644a;&#x603b;&#x989d;&#xff0c;&#x5546;&#x6237;&#x5c55;&#x4f4d;&#x53f7;)"/>
+</node>
+<node CREATED="1646819601345" ID="ID_689956558" MODIFIED="1646819822535" TEXT="&#x9000;&#x6b3e;&#x5927;&#x4fdd;&#x5b58;action">
+<node CREATED="1646819696630" ID="ID_841579195" MODIFIED="1646819714170" TEXT="application/libraries/action/casher/RefundAbstract.php"/>
+<node CREATED="1646819715612" ID="ID_448371435" MODIFIED="1646819739849" TEXT="&#x7ec4;&#x7ec7;payfrom_list&#x5305;&#x51b2;&#x7ea2;&#x6570;&#x636e;&#x65f6;&#xff0c;&#x5c06;&#x65b0;&#x52a0;&#x7684;&#x5b57;&#x6bb5;&#x586b;&#x5145;&#x5b8c;&#x6574;"/>
+</node>
+<node CREATED="1646643582386" ID="ID_1392746589" MODIFIED="1646965272271" TEXT="bll/application/libraries/action/casher/PayTypeAction.php">
+<node CREATED="1646643666098" ID="ID_973282578" MODIFIED="1646643666455" TEXT="https://docs.qq.com/doc/DSk1GWWdTRkdtVW9v"/>
+<node CREATED="1646643680709" ID="ID_641796415" MODIFIED="1646643693967" TEXT="&#x53bb;&#x6389;&#x8d2d;&#x7269;&#x5238;&#x76f8;&#x5173;&#x67e5;&#x8be2;"/>
+<node CREATED="1646643694476" ID="ID_383875143" MODIFIED="1646643735302" TEXT="&#x6dfb;&#x52a0;&#x67e5;&#x8be2;&#x7535;&#x5b50;&#x5238;&#x624b;&#x7eed;&#x8d39;&#x7a0b;&#x5e8f;(&#x5546;&#x6237;&#x5206;&#x644a;&#x91d1;&#x989d;)"/>
+</node>
+</node>
+</node>
+<node CREATED="1646968615009" ID="ID_1193681145" MODIFIED="1646968696702" POSITION="right" TEXT="&#x95ee;&#x9898;">
+<icon BUILTIN="help"/>
+<node CREATED="1646968619433" ID="ID_309196881" MODIFIED="1646968687643" TEXT="&#x4e0a;&#x7ebf;&#x540e;&#xff0c;payform_list&#x8868;&#x4e2d;&#x4e0a;&#x7ebf;&#x4e4b;&#x524d;&#x7684;&#x6570;&#x636e;&#xff0c;&#x662f;&#x5426;&#x5c5e;&#x4e8e;&#x672a;&#x5ba1;&#x6838;&#x72b6;&#x6001;&#xff0c;&#x5982;&#x679c;&#x8f93;&#x5165;&#x672a;&#x5ba1;&#x6838;&#x72b6;&#x6001;&#x5c06;&#x5f71;&#x54cd;&#x7ed3;&#x7b97;&#x64cd;&#x4f5c;"/>
+<node CREATED="1646968689514" ID="ID_1694759480" MODIFIED="1646968773832" TEXT="&#x67e5;&#x8be2;&#x5546;&#x6237;&#x65f6;&#xff0c;&#x5546;&#x6237;&#x5728;&#x79df;&#x72b6;&#x6001;&#x6709;&#xff1a;&#x5408;&#x540c;&#x72b6;&#x6001;,1 &#x5728;&#x79df;2 &#x9884;&#x79df;3 &#x8fc7;&#x671f;4 &#x4f5c;&#x5e9f;&#xff1b;&#x53ea;&#x67e5;&#x8be2;&#x5728;&#x79df;&#x72b6;&#x6001;&#x5417;&#xff1f;"/>
+</node>
+<node CREATED="1646643576026" FOLDED="true" ID="ID_561589786" MODIFIED="1646966473326" POSITION="right" TEXT="&#x4e0d;&#x505a;">
+<node CREATED="1646729349293" FOLDED="true" ID="ID_1123661857" MODIFIED="1646819805572" TEXT="&#x6536;&#x94f6;&#x7cfb;&#x7edf;&#x9000;&#x5238;&#xff0c;&#x9700;&#x8981;&#x5c06;&#x5408;&#x540c;&#x6536;&#x5238;&#x8868;&#x8d1f;&#x5411;&#x65b0;&#x589e;&#x5b57;&#x6bb5;&#x51b2;&#x7ea2;">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1646729398978" ID="ID_1243458861" MODIFIED="1646729581527" TEXT="application/libraries/action/casher/RefundAbstract.php "/>
+<node CREATED="1646729589628" ID="ID_782446941" MODIFIED="1646729699864" TEXT="320&#x884c;&#x7684;getRecord()&#x65b9;&#x6cd5;&#xff0c;&#x5c06;&#x65b0;&#x589e;&#x5b57;&#x6bb5;&#x51b2;&#x7ea2;&#x6570;&#x636e;&#x8865;&#x5145;&#x5b8c;&#x6574;"/>
+</node>
+<node CREATED="1646810300117" FOLDED="true" ID="ID_1201329714" MODIFIED="1646819814435" TEXT="&#x6536;&#x94f6;&#x7cfb;&#x7edf;&#x6536;&#x5238;&#x65f6;&#x5019;&#xff0c;&#x8c03;&#x7528;BLL&#x7528;&#x5238;&#x63a5;&#x53e3;useTicket.index_put()&#x65b9;&#x6cd5;&#x4fee;&#x6539;&#xff1a;&#x67e5;&#x8be2;&#x5546;&#x6237;&#x4fe1;&#x606f;">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1646810530956" ID="ID_1059891947" MODIFIED="1646810531723" TEXT="$this-&gt;_CI-&gt;load-&gt;library(&apos;action/common/MerchantOnlineAction&apos;,&apos;&apos;,&apos;MerchantOnlineAction&apos;);"/>
+<node CREATED="1646810543730" ID="ID_309651394" MODIFIED="1646810556501" TEXT="$merchantArr = $this-&gt;_CI-&gt;MerchantOnlineAction-&gt;get(array(&apos;orer_number&apos;=&gt;&#x5408;&#x540c;&#x53f7;));"/>
+<node CREATED="1646810571636" ID="ID_1586693509" MODIFIED="1646810635700" TEXT="&#x5f97;&#x5230;&#x5546;&#x6237;&#x4fe1;&#x606f;&#x7684;&#x5c55;&#x4f4d;&#x53f7;&#x540e;&#xff0c;&#x5728;&#x8c03;&#x7528;DAL tctCangeTicketState&#x4e8b;&#x52a1;&#x65f6;&#xff0c;&#x5c06;&#x5c55;&#x4f4d;&#x53f7;&#x4f5c;&#x4e3a;&#x5165;&#x53c2;&#x4f20;&#x8fdb;&#x8fdb;&#x53bb;"/>
+</node>
+<node CREATED="1646728574684" FOLDED="true" ID="ID_604265550" MODIFIED="1646819831728" TEXT="&#x6536;&#x94f6;&#x7cfb;&#x7edf;&#x6536;&#x5238;&#x65f6;&#x5019;&#xff0c;&#x5411;&#x5408;&#x540c;&#x6536;&#x6b3e;&#x8868;&#x65b0;&#x589e;&#x6570;&#x636e;&#xff1a;&#x5356;&#x573a;&#x5206;&#x644a;&#x6bd4;&#xff0c;&#x5356;&#x573a;&#x5206;&#x644a;&#x91d1;&#x989d;&#xff0c;&#x5546;&#x6237;&#x5206;&#x644a;&#x6bd4;&#x5b57;&#x6bb5;&#xff0c;&#x5c55;&#x4f4d;&#x53f7;">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1646728582782" ID="ID_260887581" MODIFIED="1646728780579" TEXT="application/controllers/ticket/tctChangeTicketState.php"/>
+<node CREATED="1646728793845" ID="ID_572685691" MODIFIED="1646728814923" TEXT="&#x7a0b;&#x5e8f;133&#x884c;&#x53ef;&#x4ee5;&#x53d6;&#x5230;&#x65b0;&#x589e;&#x7684;&#x6570;&#x636e;"/>
+<node CREATED="1646729162814" ID="ID_1791789621" MODIFIED="1646729180418" TEXT="&#x5176;&#x4e2d;&#x5356;&#x573a;&#x5206;&#x644a;&#x989d;=&#x5238;&#x9762;&#x989d;-&#x5546;&#x6237;&#x5206;&#x644a;&#x91d1;&#x989d;"/>
 </node>
 </node>
 </node>
